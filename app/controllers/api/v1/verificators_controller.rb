1819b40d4218ddeeea1113b359c5cc3fabb81d77
Timothy Klim <klimtimothy@gmail.com>
2013-05-18 16:53:18 +0400

    create contact by number or add to current user

--- app/controllers/api/v1/verificators_controller.rb
+++ app/controllers/api/v1/verificators_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::VerificatorsController < ApiController
     session[:phone_numbers] ||= {}
 
     if params[:phone_number].present? && valid_phone?(params[:phone_number])
-      phone_number = params[:phone_number]
+      phone_number = params[:phone_number].to_s
       if session[:phone_numbers][phone_number].present?
         render json: {status: 'already sent'}, status: 500
       else
-        code = Random.rand(899_999) + 100_000
+        code = (Random.rand(899_999) + 100_000).to_s
         session[:phone_numbers][phone_number] = code
         SendSms.perform_async(phone_number, I18n.t('verificators.phone_number', code: code))
 
@@ -21,8 +21,30 @@ class Api::V1::VerificatorsController < ApiController
   end
 
   def verification_code
-    if params[:code].present?
+    if params[:code].present? && params[:phone_number].present? && valid_phone?(params[:phone_number])
+      phone_number = params[:phone_number]
+      code = session[:phone_numbers][phone_number].to_s
+      if code.present? && params[:code].to_s == code
+        #TODO: investigate not removing phone number
+        session[:phone_numbers].delete phone_number
+        contact = Contact.where(uid: phone_number, kind_cd: Contact.kinds(:internal)).first
 
+        unless contact.present?
+          scope = if current_user.present?
+                    current_user.contacts.scoped
+                  else
+                    Contact.scoped
+                  end
+
+          scope.create!({kind: :internal, uid: phone_number}, without_protection: true)
+        end
+
+        render json: {status: 'ok'}
+      else
+        render json: {status: 'invalid code'}, status: 500
+      end
+    else
+      render json: {status: 'invalid phone number or code'}, status: 500
     end
   end
 end
