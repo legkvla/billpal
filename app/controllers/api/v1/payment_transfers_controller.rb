1ec8c275d46eb5cb44a3eb0d62c478683a2e30fb
Timothy Klim <klimtimothy@gmail.com>
2013-05-18 17:13:10 +0400

    check params

--- app/controllers/api/v1/payment_transfers_controller.rb
+++ app/controllers/api/v1/payment_transfers_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::PaymentTransfersController < ApiController
   before_filter :authenticate_user!
 
   def create
+    if params[:contact_to].present? && params[:amount].present? && params[:payment_method].present?
 
+    end
   end
 end
