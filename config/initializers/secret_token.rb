0341770387d2e6351d93bb249848283d3262b032
kuroineko <kaschenko@kaschenka.ru>
2013-05-18 11:43:35 +0400

    new rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billpal::Application.config.secret_token = '4a83cf0a6a9f65df8bf97063c202cf7fb4e003ce7022361c5e8f224c72d55b4bab58230c256e4185f66b881b3ae90ddc4c615a60c26c498b2313af12d523f728'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Billpal::Application.config.session_store :cookie_store, key: '_billpal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Billpal::Application.config.session_store :active_record_store
