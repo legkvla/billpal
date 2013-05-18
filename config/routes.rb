155e90eff933f45c5e348d071b8bde2f78a92aac
Timothy Klim <klimtimothy@gmail.com>
2013-05-19 02:32:18 +0400

    remove accounts scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 require 'sidekiq/web'
 
 Billpal::Application.routes.draw do
-
-	devise_for :users, :path => 'accounts',
-						 :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks', :registrations => 'registrations' }
+	devise_for :users,
+						 controllers: {
+                 omniauth_callbacks: 'users/omniauth_callbacks',
+                 registrations: 'registrations'
+             }
 
   get 'dashboard(/:any_action)' => 'dashboard#index', as: :dashboard
 
