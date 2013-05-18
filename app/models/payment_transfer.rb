56f82c9329ad96fec5e90a1f1e9f5840a5d67575
Timothy Klim <klimtimothy@gmail.com>
2013-05-18 13:48:17 +0400

    add contact and payment_transfer

---
+++ app/models/payment_transfer.rb
@@ -0,0 +1,9 @@
+class PaymentTransfer < ActiveRecord::Base
+  belongs_to :contact_to, class_name: 'Contact'
+  belongs_to :contact_from, class_name: 'Contact'
+
+  belongs_to :user_to, class_name: 'User'
+  belongs_to :user_from, class_name: 'User'
+
+
+end
