1ee23396283fa4ab52517818ba2c11e68ec557de
Timothy Klim <klimtimothy@gmail.com>
2013-05-18 13:08:51 +0400

    show git revision in headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_git_version
+
+  private
+
+  def set_git_version
+    response.headers['X-Git-Revision'] = GIT_HASH
+  end
 end
