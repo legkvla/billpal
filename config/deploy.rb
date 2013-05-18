edcb962a5e414659fec7af5de194dfbb42c1431d
Andrey Kouznetsov <bearonsails@gmail.com>
2013-05-18 15:25:17 +0400

    deploy conf

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+require 'bundler/deployment'
+
+set :application, "billpal"
+set :user,        "deploy"
+set :ssh_options, { :forward_agent => true }
+set :domain,      "billpal.ru"
+set :repository,  "git@github.com:wishbear/billpal.git"
+set :use_sudo,    false
+set :deploy_to,   "/webapps/#{application}"
+set :scm,         "git"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+set :rails_env, "production"
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
