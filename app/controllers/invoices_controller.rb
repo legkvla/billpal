fc83dc9dc80bfebf45823a61ca66043747a727d7
Igor Varavko <igor.varavko@gmail.com>
2013-05-19 05:53:42 +0400

    Add wisepdf

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,5 +7,12 @@ class InvoicesController < ApplicationController
 
 	def show
 		@invoice = Invoice.find(params[:id])
+
+		respond_to do |format|
+			format.html
+			format.pdf do
+				render pdf: 'invoice'
+			end
+		end
 	end
 end
