f287ebe392000278c975aced3a21068450597bfc
Andrey Kouznetsov <bearonsails@gmail.com>
2013-05-19 10:36:33 +0400

    fuck it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,11 +14,9 @@ Billpal::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = false
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.debug = true
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  # Don't fallback to assets pipeline if a precompiled asset is missed
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
